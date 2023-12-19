bff1f977886479697d0623f55f1a90b4b73903cd
syedabdulazeez <syed.abdulazeez@kreeti.com>
2023-12-19 15:00:36 +0530

    Updates

---
+++ precache-manifest.75d5e434d2a16b028184409db28b2c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fa3f8c8a0a506cd36a3b4999f1340c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1b81ff04e6bd30471a01",
+    "url": "/my-app/static/css/main.b0eb1b8d.chunk.css"
+  },
+  {
+    "revision": "4f2521801531b6e1f16c",
+    "url": "/my-app/static/js/2.9af1c38c.chunk.js"
+  },
+  {
+    "revision": "049699a563e4ca4eb947",
+    "url": "/my-app/static/js/3.2f5a7f7c.chunk.js"
+  },
+  {
+    "revision": "1b81ff04e6bd30471a01",
+    "url": "/my-app/static/js/main.ed6300b7.chunk.js"
+  },
+  {
+    "revision": "6b372a13ee021c5acd76",
+    "url": "/my-app/static/js/runtime~main.62d3290a.js"
+  }
+]);
